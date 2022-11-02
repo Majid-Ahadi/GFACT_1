f29d52212a0103f545cfdf5c7310b7aec19f60cc
Majid Ahadi <Majid_ahadi@hotmail.com>
2022-11-02 09:27:33 -0400

    Add files via upload

--- pythonprogramming3_sockets.py
+++ pythonprogramming3_sockets.py
@@ -1,5 +1,95 @@
+
 print('\n\n')
 
+
+import socket
+import _thread
+
+def handler(client_sock, address):
+	client_sock.send(b'Do you want to play a game?\n')
+	data = client_sock.recv(1024)
+	print(repr(address)+' said ' + data.decode())
+	client_sock.close()
+	print(repr(address)+ ' Connection ended. ' )
+
+
+
+server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("0.0.0.0", 1337))
+server_socket.listen(10)
+while True:
+	print('Server listening for connections ... ')
+	client_sock, address = server_socket.accept()
+	print('Connection from: '+repr(address))
+
+	_thread.start_new_thread(handler, (client_sock, address))
+
+
+
+
+
+'''
+
+# UDP Port
+import socket
+
+print('Open a terminal and type : nc -u 127.0.0.1 1337 \n')
+client_socket= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client_socket.bind(('0.0.0.0', 1337))
+
+while True:
+
+	data, addr=client_socket.recvfrom(1024)
+	print(data)
+
+
+
+---------------------------------------------------
+import socket
+
+print('Open a terminla and type "nc -nvlp 1337" in it')
+
+
+client_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect(('127.0.0.1', 1337))
+
+print('To end your chat type: End of Message \n')
+
+with open("Chat.txt",'w') as file:
+	file.close()
+
+Chat='A'
+while Chat!='end of message':
+	Chat=input('Send : ')
+	Chat=Chat.lower()
+	with open("Chat.txt",'a') as file:
+		file.write('Sent: '+Chat)
+		file.write('\n')
+		file.close()
+	client_socket.send(b" \n" +Chat.encode()+ b" \n")
+	received= client_socket.recv(1024)
+	received=str(received)
+	with open("Chat.txt",'a') as file:
+		file.write('Received: '+received)
+		file.write('\n')
+		file.close()
+	print('Received : ',received)
+
+
+client_socket.close()
+
+f=open("Chat.txt",'r')	
+content= f.read()
+print('\n\n\n Your file Content is : \n\n',content)
+
+
+
+
+
+
+----------------------------------------------------
 flag=False
 filename= input('What is the name of file that you to create?')
 try:
@@ -45,5 +135,6 @@ else:
 		print('\n\n\n Your file Content is : \n\n',content)
 
 
-
+'''
 print('\n\n')
+
