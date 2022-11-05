05c4e141c85fdecee6fc0fcca294fe6e96977cfc
Majid Ahadi <Majid_ahadi@hotmail.com>
2022-11-05 10:14:51 -0400

    Add files via upload

--- hello.c
+++ hello.c
@@ -5,7 +5,8 @@ void main()
 printf("\nHello World!\nEnter your name : ");
 int i;
 char variable_1[20];
-
+double Test=0;
+float ftest=(10/3);
 for (i=0;i<20;i++)
 	variable_1[i]=' ';
 //	printf("%c",variable_1[i]);}
@@ -24,4 +25,9 @@ printf("\nYour name is : ");
 	printf("%s",variable_1);
 
 printf(" and you are %d years old !\n",age);
+printf("Size of age = %lu\n",sizeof(age));
+printf("Size of variable 1 = %lu\n",sizeof(variable_1));
+printf("Size of i = %lu\n",sizeof(i));
+printf("Size of Test (double) = %lu\n",sizeof(Test));
+printf("%fSize of Test (float) = %lu\n",ftest,sizeof(ftest));
 }
