85a9db0a86aff5f013c44762e15b28a4bd4f20e8
Majid Ahadi <Majid_ahadi@hotmail.com>
2022-11-05 11:55:15 -0400

    Add files via upload

--- hello.c
+++ hello.c
@@ -1,13 +1,66 @@
 #include <stdio.h>
-/*
-	
-	Multi line comment is here
-	
-	
-*/	
-	
+#include <stdlib.h>
+#include <time.h>
 void main()
 {
+
+
+
+
+
+
+
+/*
+char array[]= {'H','e','l','l','o',' ','W','o','r','l','d'};
+char string[10];
+char data[20];
+printf("%s\n",array);
+printf("size of array: %lu\n\n",sizeof(array));
+
+int i=0;
+for(i=0;i<sizeof(array);i++)
+	{
+		//printf("%c\n",array[i]);
+		scanf("%c",&array[i]);
+	}
+
+
+
+printf("\n%s\n ",array);
+
+puts("Enter some data : ");
+fgets(data,sizeof data, stdin);
+printf("You entered : %s\n",data);
+
+printf("Enter New String : ");
+scanf("\n%s",&string);
+
+printf("\nThe String is : %s\n Size Of string is : %lu",string,sizeof(string));
+
+
+srand(time(NULL));
+int r = rand()%20;
+
+printf("The value of r is : %d\n",r);
+
+if (r<5){
+	printf("r is less than 5");
+	}
+	else if (r>15){
+		printf("r is greater than 15");		      
+			}
+	else{
+		printf("r is between 5 and 15");
+		}
+
+int j=0;
+int i=1;
+for(i=0;i<=r;i++){
+	printf("The i is : %d now .\n",i);
+	for(j=0;j<i;j++)
+		printf(" ");
+		}
+
 printf("\nHello World!\nEnter your name : ");
 int i;
 char variable_1[20];
@@ -36,4 +89,8 @@ printf("Size of variable 1 = %lu\n",sizeof(variable_1));
 printf("Size of i = %lu\n",sizeof(i));
 printf("Size of Test (double) = %lu\n",sizeof(Test));
 printf("%fSize of Test (float) = %lu\n",ftest,sizeof(ftest));
+
+*/
+
+printf("\n");
 }
